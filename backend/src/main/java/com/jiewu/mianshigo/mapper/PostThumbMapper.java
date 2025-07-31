72ee0d44e46b9bc5c81db443f01204ef9cad7b90
JieWu <joker_x0993@outlook.com>
2025-07-31 18:26:22 +0800

    初始化项目模板

---
+++ backend/src/main/java/com/jiewu/mianshigo/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.jiewu.mianshigo.mapper;
+
+import com.jiewu.mianshigo.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/com/jiewu/mianshigo/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.jiewu.mianshigo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jiewu.mianshigo.model.entity.User;
+
+/**
+ * 用户数据库操作
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
